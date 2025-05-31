25d26d294238e39e1b69c73df1fb511348dbdc04
Daniel Sandoval <danielsand711@gmail.com>
2025-05-31 15:15:17 -0500

    update to ensure UTF8

--- components/TacoShowcaseButton.tsx
+++ components/TacoShowcaseButton.tsx
@@ -1,14 +1,14 @@
-import Link from "next/link";
+﻿import Link from "next/link";
 
 export default function TacoShowcaseButton() {
-  return (
-    <div className="flex justify-center mt-8">
-      <Link
-        href="/Tacos"
-        className="bg-yellow-500 hover:bg-yellow-600 text-black text-lg font-bold py-3 px-8 rounded-2xl shadow-lg transition duration-200"
-      >
-        See Querrepario Tacos!
-      </Link>
-    </div>
-  );
+    return (
+        <div className="flex justify-center mt-8">
+            <Link
+                href="/Tacos"
+                className="bg-yellow-500 hover:bg-yellow-600 text-black text-lg font-bold py-3 px-8 rounded-2xl shadow-lg transition duration-200"
+            >
+                See Querrepario Tacos!
+            </Link>
+        </div>
+    );
 }
