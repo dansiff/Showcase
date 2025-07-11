d4e0c0106a54229b5bc96d14dd18b7ddb72467a3
dansiff <danielsand711@gmail.com>
2025-07-10 20:23:48 -0500

    up

--- components/TacoShowcaseButton.tsx
+++ components/TacoShowcaseButton.tsx
@@ -4,7 +4,7 @@ export default function TacoShowcaseButton() {
     return (
         <div className="flex justify-center mt-8">
             <Link
-                href="/Tacos"
+                href="/taco"
                 className="bg-yellow-500 hover:bg-yellow-600 text-black text-lg font-bold py-3 px-8 rounded-2xl shadow-lg transition duration-200"
             >
                 See Querrepario Tacos!
