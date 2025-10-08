a750458155723346fb014d3978eb4516885479bd
dan s <Daniel_sandoval711@live.com>
2025-10-08 17:03:24 -0500

    update fixed typo

---
+++ lib/supabase/server.ts
@@ -0,0 +1,13 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export const createSupabaseServerClient = async () => {
+  const cookieStore = await cookies(); // await the cookies
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: cookieStore, // pass cookieStore after awaiting
+    }
+  );
+};
